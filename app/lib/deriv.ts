d7a276d4802d2347f3ae5e926ac898b3f04f34c3
sherry williams <sherrywilliams774@gmail.com>
2026-07-14 12:44:57 -0600

    Use environment variable for redirect URI

--- app/lib/deriv.ts
+++ app/lib/deriv.ts
@@ -1,8 +1,6 @@
 export const CLIENT_ID = "33KRh8nBgBrZWFObtMHKE";
 
 export const REDIRECT_URI =
-  typeof window !== "undefined"
-    ? `${window.location.origin}/auth/callback`
-    : "";
+  process.env.NEXT_PUBLIC_REDIRECT_URI!;
 
 export const SCOPES = "trade account_manage";
